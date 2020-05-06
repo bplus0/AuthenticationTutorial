00f1f0bdd1e004f30a0b9da7c83d90547ca7d126
bplus0 <boneill1292@gmail.com>
2020-05-06 07:52:15 -0500

    done with ep 6. woofta

--- Client/Startup.cs
+++ Client/Startup.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
@@ -22,13 +29,36 @@ namespace Client
         config.DefaultChallengeScheme = "OurServer";
       })
         .AddCookie("ClientCookie")
-        .AddOAuth("OurSever", config =>
+        .AddOAuth("OurServer", config =>
         {
           config.ClientId = "client_id";
           config.ClientSecret = "client_secret";
           config.CallbackPath = "/oauth/callback";
           config.AuthorizationEndpoint = "https://localhost:44365/oauth/authorize";
           config.TokenEndpoint = "https://localhost:44365/oauth/token";
+
+          config.SaveTokens = true;
+
+          config.Events = new OAuthEvents()
+          {
+            OnCreatingTicket = context =>
+            {
+
+              var accessToken = context.AccessToken;
+              var base64Payload = accessToken.Split('.')[1];
+              var bytes = Convert.FromBase64String(base64Payload);
+              var jsonPayload = Encoding.UTF8.GetString(bytes);
+
+              var claims = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+
+              foreach (var claim in claims)
+              {
+                context.Identity.AddClaim(new Claim(claim.Key, claim.Value));
+              }
+
+              return Task.CompletedTask;
+            }
+          };
         });
 
       services.AddControllersWithViews().AddRazorRuntimeCompilation();
--- Server/Controllers/OAuthController.cs
+++ Server/Controllers/OAuthController.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Server.Controllers
 {
@@ -21,7 +28,7 @@ namespace Server.Controllers
       query.Add("state", state);
       var qs = query.ToQueryString();
 
-      return View(qs);
+      return View(model: query.ToString());
     }
 
     [HttpPost]
@@ -30,12 +37,58 @@ namespace Server.Controllers
 
       const string code = "BABABAAABA";
 
-      return Redirect($"");
+      var query = new QueryBuilder();
+      query.Add("code", code);
+      query.Add("state", state);
+
+      return Redirect($"{redirectUri}{query.ToString()}");
     }
 
-    public IActionResult Token()
+    public async Task<IActionResult> Token(string grant_type,  // flow of access_token request
+                                string code, 
+                                string redirect_uri, 
+                                string client_id)
     {
-      return View();
+
+
+      //some mechanism for validating the code
+
+      var claims = new[]
+     {
+        new Claim(JwtRegisteredClaimNames.Sub, "some_id"),
+        new Claim("granny", "cookie"),
+      };
+
+
+      var secretBytes = System.Text.Encoding.UTF8.GetBytes(Constants.Secret);
+
+      var key = new SymmetricSecurityKey(secretBytes);
+      var algorithim = SecurityAlgorithms.HmacSha256;
+
+      var signingCredentials = new SigningCredentials(key, algorithim);
+
+      var token = new JwtSecurityToken(
+        Constants.Issuer,
+        Constants.Audiance,
+        claims,
+        notBefore: DateTime.Now,
+        expires: DateTime.Now.AddDays(1),
+        signingCredentials);
+
+      var access_token = new JwtSecurityTokenHandler().WriteToken(token);
+
+      var responseObject = new
+      {
+        access_token,
+        token_type = "Bearer",
+        raw_claim = "oauthTutorial"
+      };
+
+      var responseJson = JsonConvert.SerializeObject(responseObject);
+      var responseBytes = Encoding.UTF8.GetBytes(responseJson);
+      await Response.Body.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+      return Redirect(redirect_uri);
     }
   }
 }
